1fea3c9348f4c7f0bb20233056ebc2e1b8ef3079
Christopher Sweet <crs4263@rit.edu>
2016-09-30 19:44:05 -0400

    Somehow broke csvread. Began adding in a UI

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -8,16 +8,77 @@ __author__ = "Christopher Sweet - crs4263@rit.edu"
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 import numpy as np
+import matplotlib as mpl
+from tkinter import *
+from tkinter import ttk
 import csv
 
-# Sigmoid function which determines how much to refine values by.
+class Menu():
+    """
+    Tkinter to get a basic basic UI
+    """
+
+    master = Tk()
+    master.minsize(width=800, height=450)
+    pages = ttk.Notebook(master)
+    pages.winfo_width()
+
+    openMenuFrame = Frame(pages)
+    openMenuFrame.pack()
+
+    #Contains the file name box and load button
+    upperFrame = Frame(openMenuFrame)
+    upperFrame.pack( side = TOP)
+    loadfileButton = Button(upperFrame, text = 'Load', fg = 'black')
+    loadfileButton.pack( side = RIGHT )
+    loadfileLabel = Label(upperFrame, text = 'Load a *.csv File')
+    loadfileLabel.pack( side = LEFT)
+    filenameEntry = Entry(upperFrame)
+    filenameEntry.pack(side = BOTTOM)
+
+    #Contains the Run Button
+    lowerFrame = Frame(openMenuFrame)
+    lowerFrame.pack( side = BOTTOM)
+    runButton = Button(lowerFrame, text = 'Run', fg = 'black')
+    runButton.pack( side = BOTTOM)
+
+    #Adds the first page in
+    pages.add(openMenuFrame, text="Load")
+
+    showDataFrame = Frame(pages)
+    showDataFrame.pack()
+
+    # Contains the Predict Button & Return Button
+    lowerFrame2 = Frame(showDataFrame)
+    lowerFrame2.pack(side=BOTTOM)
+    returnButton = Button(lowerFrame2, text='Return', fg='black')
+    returnButton.pack(side=LEFT)
+    predictButton = Button(lowerFrame2, text='Predict', fg='black')
+    predictButton.pack(side=RIGHT)
+
+    #Adds the second page in
+    pages.add(showDataFrame, text="Visualize")
+
+    pages.pack()
+    master.mainloop()
+
 def nonlin(x,deriv = False):
+    """
+    Defines the sigmoid function used in refining matrix values
+    :param x: Value to refine
+    :param deriv: Flag to take Derivative
+    :return: Value refined
+    """
     if(deriv == True):
         return x*(1-x)
     return 1/(1+np.exp(-x))
 
-# Read in .csv file that contains biking data
 def readData(fname):
+    """
+    Read in *.csv file that contains the appropriate biking data
+    :param fname: File to read in
+    :return: Input times, Output distances
+    """
     with open(fname, newline='') as csvfile:
         dataReader = csv.reader(csvfile, delimiter = ',', quotechar = '|')
         inputArray = np.array([[]])
@@ -28,8 +89,13 @@ def readData(fname):
     csvfile.close()
     return inputArray, outputArray
 
-# Make a prediction based off the learned values
 def predict(model, time):
+    """
+    Make a prediciton based off the learned values
+    :param model: Trained Neural Network Parameters
+    :param time: Time to produce a distance estimate for
+    :return: Distance Estimate
+    """
     maxVals, syn0, syn1 = model['maxVals'], model['syn0'], model['syn1']
     # Scale down into unit scalar
     time /= maxVals[0]
@@ -43,63 +109,68 @@ def predict(model, time):
     # Return the value predicted
     return matTime[0][0][0]
 
-# Scale the values down to Unit Scalars - Helper Function
 def scale_down(array0, array1, maxVals):
+    """
+    Scales the values down into unit measurements
+    :param array0: First array to scale down
+    :param array1: Second array to scale down
+    :param maxVals: Maximum values to use for unit scaling
+    :return: Scaled values
+    """
     loop = np.nditer(array0, op_flags=['readwrite'], flags=['f_index'])
     while not loop.finished:
         array0[loop.index] = (array0[loop.index])/(maxVals[0])
         array1[loop.index] = (array1[loop.index])/(maxVals[1])
         loop.iternext()
 
-# Scale the values back up to distances - Helper Function
 def scale_up(array, prediction, maxVals):
+    """
+    Scales the values back up to distances
+    :param array: Array of unit scalars
+    :param prediction: Predicted value
+    :param maxVals: Maximum values to use in scaling
+    :return: Array Scaled back into Distance
+    """
     loop = np.nditer(array, op_flags = ['readwrite'], flags = ['f_index'])
     while not loop.finished:
         array[loop.index] = prediction[loop.index]*maxVals[1]
         loop.iternext()
 
 def create_model(fname):
+    """
+    Creates and trains a 3-layer Neural Network for cycling times/distances
+    :param fname: File to load in
+    :return: Dictionary of weighted Neural Network Parameters
+    """
     # Reads the training data from csv
     inputData, outputData = readData(fname)
-
     # Input Data - Duration of Ride (min)
     inputData = inputData.T
-
     # Output Data - Distance traveled (Mi)
     outputData = outputData.T
-
-    # Store the max values, and use them to scale the data
+    # Store the max values, and use them to normalize the data
     maxVals = np.array([[np.amax(inputData)], [np.amax(outputData)]])
-
     # Scale input times and output distances
     scale_down(inputData, outputData, maxVals)
-
     # Seed Random values to start with
     np.random.seed(1)
-
     # Create the synaptic connections between neurons.
     synapse0 = 2 * np.random.random((1, 10)) - 1
     synapse1 = 2 * np.random.random((10, 1)) - 1
-
     # Training the neural network. Number of runs influences results only to a certain point.
     for t in range(0, 50000):
         # Feed data forward from input to output
         layer0 = inputData
         layer1 = nonlin(layer0.dot(synapse0))
         layer2 = nonlin(layer1.dot(synapse1))
-
         # Determine how far off predicted values were from the training ones
         layer2_error = outputData - layer2
-
         # Calculate the amount to modify layer 2 weights by
         layer2_delta = layer2_error * nonlin(layer2, True)
-
         # Calculates the amount of error resultant from layer 1 (Look into how this actually accomplishes this)
         layer1_error = layer2_delta.dot(synapse1.T)
-
         # Calculate the amount to change layer 1 weights by
         layer1_delta = layer1_error * nonlin(layer1, True)
-
         # Update synaptic weights
         synapse1 += layer1.T.dot(layer2_delta)
         synapse0 += layer0.T.dot(layer1_delta)
@@ -108,7 +179,8 @@ def create_model(fname):
     # Return an anonymous dictionary of the weighted network values
     return { 'maxVals': maxVals, 'syn1': synapse1, 'syn0': synapse0}
 
-fname = input("Enter a csv filename: ")
-neuralNetwork = create_model(fname)
-distance = predict(neuralNetwork, 60)
-print("Based off your previous data you should be able to ride", distance, "miles!")
+menu = Menu()
+#fname = input("Enter a csv filename: ")
+#neuralNetwork = create_model(fname)
+#distance = predict(neuralNetwork, 60)
+#print("Based off your previous data you should be able to ride", distance, "miles!")
